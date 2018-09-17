df34657a34c75f42cc85d77c9f618a95aa2bc319
Josh Bereson <joshbereson@gmail.com>
2018-09-17 10:54:30 +1000

    Fixed Lochies mistake...

---
+++ week 8/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week8
+{
+	public abstract class Weapon
+	{
+		public int baseDamage;
+		public int baseRange;
+		public int actionPoints;
+		public int criticalDamage;
+
+		void poke()
+		{
+			//does 1 damage
+		}
+	}
+
+	public class Ranged : Weapon
+	{
+
+	}
+
+	public class Melee : Weapon
+	{
+
+	}
+}
